6c9faaac33b0fbc80fd957f00f1f660acc49716d
clouwood-ivoyant <clouwood@ivoyant.com>
2022-10-28 13:37:30 +0530

    Firebase Empty Configuration

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 
 const firebaseConfig = {
   apiKey: "xxxxxxxxxxxxxx--xxxxxxxxxxxxxxxxxxxxxxx",
@@ -19,6 +16,4 @@ const firebaseConfig = {
   measurementId: "G-xxxxxxxxxx",
 };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
-export default getStorage(app);
+initializeApp(firebaseConfig);
